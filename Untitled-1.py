7bb9c1b3ea1954e37c1318f1180c2e81966bc800
richietaylor <richard@taylorfamily.co.za>
2023-04-19 15:48:40 +0200

    Added random number generator and dummy methods

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,3 +1,51 @@
 # TYLRIC007 - Richard Taylor
 # OS Assignment 1 - Paging Techniques
-# 13/04/2023 -
+
+from random import randint
+import sys
+
+
+def FIFO(size, pages):
+    # keep track of page faults
+    faults = 0
+    return faults
+
+
+def LRU(size, pages):
+    # keep track of page faults
+    faults = 0
+    return faults
+
+
+def OPT(size, pages):
+    # keep track of page faults
+    faults = 0
+    return faults
+
+
+def main():
+
+    # command line argument sets the number of pages to be referenced in sequence
+    pageLength = int(sys.argv[1])
+
+    # page size - can be manually set from 1 to 7
+    size = 7
+
+    # Random number generator -
+    # generates a list of random ints from 0 to 9
+    pages = []
+    for x in range(pageLength):
+        pages.append(randint(0, 9))
+    print(pages[:])
+
+    # call to our paging algorithms
+    print("FIFO", FIFO(size, pages), 'page faults.')
+    print('LRU', LRU(size, pages), 'page faults.')
+    print('OPT', OPT(size, pages), 'page faults.')
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python paging.py [number of pages]")
+    else:
+        main()
